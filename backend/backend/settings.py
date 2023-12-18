e339b7ee35109020f209139de175b164562d4693
meri109 <mehriban207@gmail.com>
2023-12-18 23:37:24 +0400

    login register

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # INTERNAL
+    'song',
+    'user',
+    # EXTERNAL
     'rest_framework',
     "corsheaders",
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +136,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
---
+++ backend/song/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Genre(models.Model):
+    title = models.CharField(50)
+    # image = models.ImageField(upload_to='genres/images/')
+    updated = models.DateField(auto_now = True)
+    created = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Playlist(models.Model):
+    title = models.TextField()
+    author = models.ForeignKey('user.Customer', on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='playlists/images/')
+    featured = models.BooleanField(default=False)
+    updated = models.DateField(auto_now = True)
+    created = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+class Song(models.Model):
+    title = models.TextField()
+    image = models.ImageField(upload_to='songs/images/')
+    audio = models.FileField(upload_to='songs/audios/')
+    artist = models.ManyToManyField('user.Artist')
+    duration = models.IntegerField()
+    lyrics = models.TextField(null=True, blank= True)
+    genres = models.ManyToManyField(Genre)
+    playlists = models.ManyToManyField(Playlist, blank=True)
+    listen_count = models.BigIntegerField(default=0)
+    updated = models.DateField(auto_now = True)
+    created = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
